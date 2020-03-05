5e01c0d8d5cd53da22adeb066d705671f742c8e8
Thiago Castro <thiago.mael@gmail.com>
2020-03-04 21:42:41 -0300

    Variability encoding of expressions using the abstract derivation combinator
    I left some `println()`s along the way to visualize the process of
    composing expressions while substituting the original variables (RDG
    node ids) for aliases of equivalence classes for presence conditions.
    
    For instance, take two RDG nodes, identified by `n6` and `n8`, whose
    presence conditions are the same (e.g., `Keys` for the feature with the
    same name in the Email SPL).
    In this case, both variables are replaced with the same "new" variable
    during variability encoding (e.g., `s0`).
    
    These new variables use a different naming convention to avoid clashes
    (due to non-associative composition) and, at the same time, suggest that
    the semantics is also changed (from Real-valued reliabilities to
    Boolean-valued presence indicators).

--- src/tool/analyzers/buildingblocks/ExpressionsVariabilityEncoding.java
+++ src/tool/analyzers/buildingblocks/ExpressionsVariabilityEncoding.java
@@ -1,9 +1,49 @@
 package tool.analyzers.buildingblocks;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+/**
+ * Responsible for variability encoding of expressions (gamma).
+ *
+ */
 public class ExpressionsVariabilityEncoding {
+
+    private static DerivationFunction<String, String, String> derive150Model =
+            DerivationFunction.abstractDerivation(ExpressionsVariabilityEncoding::ifThenElse,
+                                                  ExpressionsVariabilityEncoding::inline,
+                                                  "1");
+
     public static String encodeVariability(List<Component<String>> sortedExpressions) {
-        return "1";
+        List<String> presenceConditions = sortedExpressions.stream()
+                .map(Component::getPresenceCondition)
+                .collect(Collectors.toList());
+
+        Map<String, String> pcEquivalence = PresenceConditions.toEquivalenceClasses(presenceConditions);
+        String derived150Model = Component.deriveFromMany(sortedExpressions,
+                                                          derive150Model,
+                                                          c -> pcEquivalence.get(c.getPresenceCondition()));
+        return derived150Model;
+    }
+
+    private static String ifThenElse(String variable,
+                                     String ifTrueExpr,
+                                     String ifFalseExpr) {
+        return variable + "*(" + ifTrueExpr + ")"
+             + " + (1 - " + variable + ")*(" + ifFalseExpr + ")";
+    }
+
+    private static String inline(String expression, Map<String, String> toBeEncoded) {
+        System.out.println(">>>>>>>>>>>>>>>>>>>");
+        System.out.println("Inlining: "+expression);
+        String inlined = expression;
+        for (Map.Entry<String, String> entry: toBeEncoded.entrySet()) {
+            inlined = inlined.replaceAll("\\b"+entry.getKey()+"\\b",
+                                         "(" + entry.getValue() + ")");
+            System.out.println("    " + inlined);
+        }
+        System.out.println("-------------------");
+        return inlined;
     }
 }
